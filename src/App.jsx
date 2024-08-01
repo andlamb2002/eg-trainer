f41574a42a33e253735df6a8aafe8d3369f31063
Andreas Lambropoulos <andlamb2002@gmail.com>
2024-07-31 23:54:57 -0400

    Broken case selection

--- src/App.jsx
+++ src/App.jsx
@@ -4,70 +4,148 @@ import ScrambleDisplay from './Components/ScrambleDisplay';
 import TimeList from './Components/TimeList';
 import RightSidebar from './Components/RightSidebar';
 
-const scrambles = [
-  // "R U R' U R U R2 F R2 U' R'",
-  // "F2 U' F' U' F U' F' U F'",
-  // "F U2 F U2 F U F2",
-  // "R U R' F' U' R U R' U' F R' F' R",
-  // "F' R2 F' R2 F' U' F2 R2",
-  // "F R U' R' F U' F' R' F' R",
-  // "F' U2 R U2 R' U2 F",
-  // "R U R' U F' R U R' U' R U R2 F2 R",
-  // "F' U' F R' F2 R F' U F",
-  // "R U' R' F R U' R2 F R",
-  // "R' F R F' R' F R2 U' R'",
-  // "R U' R' F R U' R2 F R",
-  // "R' F R F' U R U' R' F R U' R'",
-  // "F U' R' F2 R F' U2 F'",
-  // "F2 R U2 R' U' F2 U2 F'",
-  // "R' F R U' R' F R U' R U R' F'",
-  // "R' F R2 U' R' U F U F'",
-  // "F' U R U2 R' F U2 F",
-
-  // "R2 U R U' R2 F R U2 R' F",
-  // "R U2 R' U' R' F' R F R' F' R",
-  // "R2 U R U' R2 F R U2 R' F",
-  // "R U2 R' F R U' R' F' R U R'",
-  // "R U' R' F R U' R' F R U R' F'",
-  // "F' R' F R2 U R' U' R U R'",
-  // "R U2 R' U' R' F' R2 U R'",
-  // "R' F R2 U' R' U' R' F2 R",
-  // "R U' R2 F R U R U2 R'",
-  // "R' F R F' U R U' R' U F R U' R'",
-  // "R' U F R2 U' R2 U' F U' R",
-  // "R' F R F' U R U' R' U F R U' R'",
-  // "R' F' R2 U R' F' R U R'",
-  // "R U R2 F' R F R' F' R",
-  // "R' F' R2 U R' F' R U R'",
-  // "R' U' R U F2 U' F2 R U R",
-  // "R U R' F R U R' F U' R U' R'",
-  // "R U' R' U2 F R U2 R' F",
-
-  "R U' R' U R U' R2 F' R F",
-  "R U R' F' R U2 R' U2 R U R'",
-  "F R U' R' F' R U R' F' R U R'",
-  "R' F R U' R' F R2 U R' F'",
-  "R' F R F' R' F R U R U2 R'",
-  "R' F' R F' R' F' R F' R' F2 R",
-  "R' U R2 U' R2 U' F R2 U' R'",
-  "R U R2 F' R2 U' R2 U' R2 U' R2 U2 R",
-  "R' U' R U' R' U2 F R2 U' R' U' R' F2 R",
-  "R U2 R' F R U' R2 F' R",
-  "R' F R2 U R' F' R U2 R'",
-  "R U2 R' F R U' R2 F' R",
-  "R U R' F' R U R' U' F R' F' R",
-  "F' R' F R U' R U R' U' R U R'",
-  "R' F R F U2 F U' F2",
-  "F R U' R' U R' F' R U R' F' R",
-  "R' U2 F R U2 R U' R2 F",
-  "R' F' R F U' R' F' R U R' F' R"
-];
+const scrambles = {
+  U: [
+    "R U R' U R U R2 F R2 U' R'",
+    "F2 U' F' U' F U' F' U F'",
+    "F U2 F U2 F U F2",
+    "R U R' F' U' R U R' U' F R' F' R",
+    "F' R2 F' R2 F' U' F2 R2",
+    "F R U' R' F U' F' R' F' R",
+    "F' U2 R U2 R' U2 F",
+    "R U R' U F' R U R' U' R U R2 F2 R",
+    "F' U' F R' F2 R F' U F",
+    "R U' R' F R U' R2 F R",
+    "R' F R F' R' F R2 U' R'",
+    "R U' R' F R U' R2 F R",
+    "R' F R F' U R U' R' F R U' R'",
+    "F U' R' F2 R F' U2 F'",
+    "F2 R U2 R' U' F2 U2 F'",
+    "R' F R U' R' F R U' R U R' F'",
+    "R' F R2 U' R' U F U F'",
+    "F' U R U2 R' F U2 F"
+  ],
+  T: [
+    "R2 U R U' R2 F R U2 R' F",
+    "R U2 R' U' R' F' R F R' F' R",
+    "R2 U R U' R2 F R U2 R' F",
+    "R U2 R' F R U' R' F' R U R'",
+    "R U' R' F R U' R' F R U R' F'",
+    "F' R' F R2 U R' U' R U R'",
+    "R U2 R' U' R' F' R2 U R'",
+    "R' F R2 U' R' U' R' F2 R",
+    "R U' R2 F R U R U2 R'",
+    "R' F R F' U R U' R' U F R U' R'",
+    "R' U F R2 U' R2 U' F U' R",
+    "R' F R F' U R U' R' U F R U' R'",
+    "R' F' R2 U R' F' R U R'",
+    "R U R2 F' R F R' F' R",
+    "R' F' R2 U R' F' R U R'",
+    "R' U' R U F2 U' F2 R U R",
+    "R U R' F R U R' F U' R U' R'",
+    "R U' R' U2 F R U2 R' F"
+  ],
+  L: [
+    "R U' R' U R U' R2 F' R F",
+    "R U R' F' R U2 R' U2 R U R'",
+    "F R U' R' F' R U R' F' R U R'",
+    "R' F R U' R' F R2 U R' F'",
+    "R' F R F' R' F R U R U2 R'",
+    "R' F' R F' R' F' R F' R' F2 R",
+    "R' U R2 U' R2 U' F R2 U' R'",
+    "R U R2 F' R2 U' R2 U' R2 U' R2 U2 R",
+    "R' U' R U' R' U2 F R2 U' R' U' R' F2 R",
+    "R U2 R' F R U' R2 F' R",
+    "R' F R2 U R' F' R U2 R'",
+    "R U2 R' F R U' R2 F' R",
+    "R U R' F' R U R' U' F R' F' R",
+    "F' R' F R U' R U R' U' R U R'",
+    "R' F R F U2 F U' F2",
+    "F R U' R' U R' F' R U R' F' R",
+    "R' U2 F R U2 R U' R2 F",
+    "R' F' R F U' R' F' R U R' F' R"
+  ],
+  P: [
+    "F U' R' F R U' F2 R U R'",
+    "F2 R U R' U2 R U R' U' F",
+    "F U' R' F R U' F2 R U R'",
+    "R' F R2 U' R2 F R",
+    "F U F' U' F U2 F U' F2",
+    "R U' R2 F R2 U' R'",
+    "F' R U2 R' F' U2 F R' F' R",
+    "F R' F U' F2 R U R",
+    "F' R U2 R' F' U2 F R' F' R",
+    "F' R U R' U' R U R' F' R U R'",
+    "R U' R' U R U' R' F R U' R'",
+    "F' R U R' U' R U R' F' R U R'",
+    "R U' R2 F R U R U' R' U' R' F R F'",
+    "F U' R U2 R' F' R U R' F'",
+    "R' U' R' F2 U' R U2 F2 R"
+  ],
+  H: [
+    "R U' R2 F R F' R' F' R F",
+    "F' R' F R F R' F' R2 U R'",
+    "F' R2 F' R F R' U R'",
+    "F2 U' F U' F' U F' U' F U' F2",
+    "F R U' R2 F U' F2 U R",
+    "R U' R' U' R U' R2 F2 R U R U R'",
+    "R' U' R' F2 U F' R F'",
+    "F U2 R U' R' F2 R' F2 R F'",
+    "R U' R' F U2 F' R' F2 R F'",
+    "F' U R' F R F' U F2",
+    "R' F R F' R' F R U' R' F R F'",
+    "F' U2 F' U R' F' R F'"
+  ],
+  S: [
+    "R U' R2 F' R F U R' F R",
+    "R U R' U F R U' R2 F' R",
+    "R U' R2 F' R F U R' F R",
+    "R' F R F U F2 R' F R",
+    "F R' F' R F R U' R' U R' F' R",
+    "R U R' F2 U F R U R'",
+    "R' F R U2 R U' R2 F2 R F'",
+    "F R' F' R U R' F' R2 U R'",
+    "R' F R U2 R U' R2 F2 R F'",
+    "F' U R U' R' U F R U R'",
+    "F' R' F R2 U R' U' F R' F' R",
+    "F' U R U' R' U F R U R'"
+  ],
+  A: [
+    "R' F' R U' F' R' F R2 U R'",
+    "R' F R2 U R' F' U' R U' R'",
+    "R' F' R U' F' R' F R2 U R'",
+    "R U' F2 R U2 R U' F",
+    "F R U' R' U R' F' R U F' R U R'",
+    "R U' R' F' U' F2 R U' R'",
+    "R U' R' U2 R' F R2 U2 R' F",
+    "F' R U R' U' R U R2 F' R",
+    "R U' R' U2 R' F R2 U2 R' F",
+    "R' F R F' U R U' R2 F' R F",
+    "R U R' U2 R' F' R F R' F R",
+    "R U' R' F' U' R U R' U' F",
+    "R' F R U' R' F R U R U R' F'",
+    "R' F2 R U' R U R' F' R U' R'",
+    "F' R' F R U R U R' U' R U R'",
+    "R F' U' F R' U' F' R2 U R2 F' R",
+    "R U' R2 F R U' R' F R F'",
+    "R F' U' F R' U' F' R2 U R2 F' R"
+  ]
+};
 
 const App = () => {
-  const initialScramble = scrambles[Math.floor(Math.random() * scrambles.length)];
+  const initialScramble = scrambles.P[Math.floor(Math.random() * scrambles.P.length)];
   const [currentScramble, setCurrentScramble] = useState(initialScramble);
   const [alteredScramble, setAlteredScramble] = useState('');
   const [times, setTimes] = useState([]);
+  const [caseToggles, setCaseToggles] = useState({
+    P: true,
+    H: true,
+    S: true,
+    A: true,
+    T: true,
+    U: true,
+    L: true,
+  });
 
   const transformScramble = (scramble) => {
     const invertScramble = (scramble) => {
@@ -101,9 +179,26 @@ const App = () => {
     setAlteredScramble(transformScramble(currentScramble));
   }, [currentScramble]);
 
+  useEffect(() => {
+    updateScramble();
+  }, [caseToggles]);
+
   const updateScramble = () => {
-    const newScramble = scrambles[Math.floor(Math.random() * scrambles.length)];
+    const enabledCases = Object.keys(caseToggles).filter(key => caseToggles[key]);
+    const randomCase = enabledCases[Math.floor(Math.random() * enabledCases.length)];
+    const newScramble = scrambles[randomCase][Math.floor(Math.random() * scrambles[randomCase].length)];
     setCurrentScramble(newScramble);
+  };
+
+  const toggleCase = (caseName) => {
+    setCaseToggles((prevToggles) => {
+      const newToggles = { ...prevToggles, [caseName]: !prevToggles[caseName] };
+      const enabledCases = Object.values(newToggles).filter(val => val).length;
+      if (enabledCases === 0) {
+        return prevToggles; // Ensure at least one case is enabled
+      }
+      return newToggles;
+    });
   };
 
   return (
@@ -118,7 +213,13 @@ const App = () => {
         <Timer onStop={updateScramble} times={times} setTimes={setTimes} />
       </main>
       <aside className="col-span-1 bg-gray-200">
-        <RightSidebar scramble={alteredScramble} times={times} setTimes={setTimes} />
+        <RightSidebar 
+          scramble={alteredScramble} 
+          times={times} 
+          setTimes={setTimes} 
+          caseToggles={caseToggles}
+          toggleCase={toggleCase}
+        />
       </aside>
     </div>
   );
--- src/Components/RightSidebar.jsx
+++ src/Components/RightSidebar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const RightSidebar = ({ scramble, times, setTimes }) => {
+const RightSidebar = ({ scramble, times, setTimes, caseToggles, toggleCase }) => {
   const lastSolveTime = times.length > 0 ? (times[times.length - 1] / 1000).toFixed(3) : null;
 
   const removeLastSolve = () => {
     setTimes(times.slice(0, -1));
   };
+
+  const caseButtons = ["U", "T", "L", "P", "H", "S", "A"];
 
   return (
     <aside>
@@ -17,9 +19,19 @@ const RightSidebar = ({ scramble, times, setTimes }) => {
           <p>Scramble: {scramble}</p>
         </div>
       ) : (
-        <div>
-        </div>
+        <div></div>
       )}
+      <div>
+        {caseButtons.map(caseName => (
+          <button
+            key={caseName}
+            onClick={() => toggleCase(caseName)}
+            style={{ backgroundColor: caseToggles[caseName] ? 'lightgreen' : 'lightcoral' }}
+          >
+            {caseName} Cases
+          </button>
+        ))}
+      </div>
     </aside>
   );
 };
