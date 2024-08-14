af7112c1f16af61b51726405cea4f1f98bb0137c
Andreas Lambropoulos <andlamb2002@gmail.com>
2024-08-14 01:45:56 -0400

    Separated scramble gen logic to new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,184 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import { scrambles, transformScramble } from './Components/Scrambles';
 import ScrambleDisplay from './Components/ScrambleDisplay';
 import Timer from './Components/Timer';
 import RightSidebar from './Components/RightSidebar';
 import LeftSidebar from './Components/LeftSidebar';
 
-const scrambles = {
-  U: [
-    "R U R' U R U R2 F R2 U' R'",
-    "F2 U' F' U' F U' F' U F'",
-    "F U2 F U2 F U F2",
-    "R U R' F' U' R U R' U' F R' F' R",
-    "F' R2 F' R2 F' U' F2 R2",
-    "F R U' R' F U' F' R' F' R",
-    "F' U2 R U2 R' U2 F",
-    "R U R' U F' R U R' U' R U R2 F2 R",
-    "F' U' F R' F2 R F' U F",
-    "R U' R' F R U' R2 F R",
-    "R' F R F' R' F R2 U' R'",
-    "R U' R' F R U' R2 F R",
-    "R' F R F' U R U' R' F R U' R'",
-    "F U' R' F2 R F' U2 F'",
-    "F2 R U2 R' U' F2 U2 F'",
-    "R' F R U' R' F R U' R U R' F'",
-    "R' F R2 U' R' U F U F'",
-    "F' U R U2 R' F U2 F"
-  ],
-  T: [
-    "R2 U R U' R2 F R U2 R' F",
-    "R U2 R' U' R' F' R F R' F' R",
-    "R2 U R U' R2 F R U2 R' F",
-    "R U2 R' F R U' R' F' R U R'",
-    "R U' R' F R U' R' F R U R' F'",
-    "F' R' F R2 U R' U' R U R'",
-    "R U2 R' U' R' F' R2 U R'",
-    "R' F R2 U' R' U' R' F2 R",
-    "R U' R2 F R U R U2 R'",
-    "R' F R F' U R U' R' U F R U' R'",
-    "R' U F R2 U' R2 U' F U' R",
-    "R' F R F' U R U' R' U F R U' R'",
-    "R' F' R2 U R' F' R U R'",
-    "R U R2 F' R F R' F' R",
-    "R' F' R2 U R' F' R U R'",
-    "R' U' R U F2 U' F2 R U R",
-    "R U R' F R U R' F U' R U' R'",
-    "R U' R' U2 F R U2 R' F"
-  ],
-  L: [
-    "R U' R' U R U' R2 F' R F",
-    "R U R' F' R U2 R' U2 R U R'",
-    "F R U' R' F' R U R' F' R U R'",
-    "R' F R U' R' F R2 U R' F'",
-    "R' F R F' R' F R U R U2 R'",
-    "R' F' R F' R' F' R F' R' F2 R",
-    "R' U R2 U' R2 U' F R2 U' R'",
-    "R U R2 F' R2 U' R2 U' R2 U' R2 U2 R",
-    "R' U' R U' R' U2 F R2 U' R' U' R' F2 R",
-    "R U2 R' F R U' R2 F' R",
-    "R' F R2 U R' F' R U2 R'",
-    "R U2 R' F R U' R2 F' R",
-    "R U R' F' R U R' U' F R' F' R",
-    "F' R' F R U' R U R' U' R U R'",
-    "R' F R F U2 F U' F2",
-    "F R U' R' U R' F' R U R' F' R",
-    "R' U2 F R U2 R U' R2 F",
-    "R' F' R F U' R' F' R U R' F' R"
-  ],
-  P: [
-    "F U' R' F R U' F2 R U R'",
-    "F2 R U R' U2 R U R' U' F",
-    "F U' R' F R U' F2 R U R'",
-    "R' F R2 U' R2 F R",
-    "F U F' U' F U2 F U' F2",
-    "R U' R2 F R2 U' R'",
-    "F' R U2 R' F' U2 F R' F' R",
-    "F R' F U' F2 R U R",
-    "F' R U2 R' F' U2 F R' F' R",
-    "F' R U R' U' R U R' F' R U R'",
-    "R U' R' U R U' R' F R U' R'",
-    "F' R U R' U' R U R' F' R U R'",
-    "R U' R2 F R U R U' R' U' R' F R F'",
-    "F U' R U2 R' F' R U R' F'",
-    "R' U' R' F2 U' R U2 F2 R",
-    "R' F R F' R' F R2 U R' U' R U' R'",
-    "F R U' R' F R U2 R' U F'",
-    "F R' F' R U R U R' U' R' F' R2 U R'"
-  ],
-  H: [
-    "R U' R2 F R F' R' F' R F",
-    "F' R' F R F R' F' R2 U R'",
-    "F' R2 F' R F R' U R'",
-    "F2 U' F U' F' U F' U' F U' F2",
-    "F R U' R2 F U' F2 U R",
-    "R U' R' U' R U' R2 F2 R U R U R'",
-    "R' U' R' F2 U F' R F'",
-    "F U2 R U' R' F2 R' F2 R F'",
-    "R U' R' F U2 F' R' F2 R F'",
-    "F' U R' F R F' U F2",
-    "R' F R F' R' F R U' R' F R F'",
-    "F' U2 F' U R' F' R F'"
-  ],
-  S: [
-    "R U' R2 F' R F U R' F R",
-    "R U R' U F R U' R2 F' R",
-    "R U' R2 F' R F U R' F R",
-    "R' F R F U F2 R' F R",
-    "F R' F' R F R U' R' U R' F' R",
-    "R U R' F2 U F R U R'",
-    "R' F R U2 R U' R2 F2 R F'",
-    "F R' F' R U R' F' R2 U R'",
-    "R' F R U2 R U' R2 F2 R F'",
-    "F' U R U' R' U F R U R'",
-    "F' R' F R2 U R' U' F R' F' R",
-    "F' U R U' R' U F R U R'",
-    "R2 F U' R U' R U' F2",
-    "R U' R' U R U' R' U F R U' R'",
-    "R2 F U' R U' R U' F2",
-    "R' F R2 U' R2 F U R F' U F R'",
-    "R' F R2 U' R' U R U' R' F",
-    "R' F R2 U' R2 F U R F' U F R'"
-  ],
-  A: [
-    "R' F' R U' F' R' F R2 U R'",
-    "R' F R2 U R' F' U' R U' R'",
-    "R' F' R U' F' R' F R2 U R'",
-    "R U' F2 R U2 R U' F",
-    "F R U' R' U R' F' R U F' R U R'",
-    "R U' R' F' U' F2 R U' R'",
-    "R U' R' U2 R' F R2 U2 R' F",
-    "F' R U R' U' R U R2 F' R",
-    "R U' R' U2 R' F R2 U2 R' F",
-    "R' F R F' U R U' R2 F' R F",
-    "R U R' U2 R' F' R F R' F R",
-    "R U' R' F' U' R U R' U' F",
-    "R' F R U' R' F R U R U R' F'",
-    "R' F2 R U' R U R' F' R U' R'",
-    "F' R' F R U R U R' U' R U R'",
-    "R F' U' F R' U' F' R2 U R2 F' R",
-    "R U' R2 F R U' R' F R F'",
-    "R F' U' F R' U' F' R2 U R2 F' R"
-  ]
-};
-
 const App = () => {
   const [currentScramble, setCurrentScramble] = useState('');
   const [alteredScramble, setAlteredScramble] = useState('');
-  const [previousScramble, setPreviousScramble] = useState('');
   const [currentCase, setCurrentCase] = useState(''); // State to track the current case type
   const [solveTimes, setSolveTimes] = useState([]);
   const [caseToggles, setCaseToggles] = useState({
     U: true, T: true, L: true, P: true, H: true, S: true, A: true,
   });
   const [selectedSolve, setSelectedSolve] = useState(null);
-
-  const transformScramble = (scramble) => {
-    const invertScramble = (scramble) => {
-      if (!scramble) return '';
-      let moves = scramble.split(' ');
-      let inversedMoves = moves.map(move => {
-        if (move.includes("'")) {
-          return move.replace("'", "");
-        } else if (move.includes("2")) {
-          return move;
-        } else {
-          return move + "'";
-        }
-      });
-      return inversedMoves.reverse().join(' ');
-    };
-
-    const addRandomU = (moveSet) => {
-      const options = ["", "U", "U'", "U2"];
-      const start = options[Math.floor(Math.random() * options.length)];
-      const end = options[Math.floor(Math.random() * options.length)];
-      return `${start} ${moveSet} ${end}`.trim();
-    };
-
-    const inversed = invertScramble(scramble);
-    const newScramble = addRandomU(inversed);
-    return newScramble;
-  };
 
   // Timer states
   const [timer, setTimer] = useState(0);
@@ -252,7 +87,6 @@ const App = () => {
 
   const updateScramble = () => {
     const newScramble = generateNewScramble();
-    setPreviousScramble(alteredScramble);
     setCurrentScramble(newScramble);
   };
 
